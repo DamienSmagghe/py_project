9c38262e3e565df303103e8e5748fa933ed46684
songtoan272 <toancs272@gmail.com>
2020-07-18 20:58:53 +0200

    simplified model done

--- simplified_model.py
+++ simplified_model.py
@@ -6,14 +6,15 @@ import matplotlib.animation as animation
 import math
 from copy import deepcopy
 
+
 # DEFINE GLOBAL PARAMETERS OF THE MODEL
-N = 25  # number of neurons in the network
-threshold = 60  # the threshold of activation of neuron, in mV, used in functions
-Vmax = 120  # the potential of neuron when pass the seuil
-Vmin = -10  # the min potential of neuron
-nb_steps = 500  # number of simulation
-beta = 0.6  # the lost coefficient when transmitting potential from a neuron to another
-gamma = 0.9  # the coefficient of leaking potential
+# N = 25  # number of neurons in the network
+# threshold = 60  # the threshold of activation of neuron, in mV, used in functions
+# Vmax = 120  # the potential of neuron when pass the seuil
+# Vmin = -10  # the min potential of neuron
+# nb_steps = 500  # number of simulation
+# beta = 0.6  # the lost coefficient when transmitting potential from a neuron to another
+# gamma = 0.9  # the coefficient of leaking potential
 
 
 class SimplifiedModel:
@@ -60,9 +61,9 @@ class SimplifiedModel:
     gamma = property(__get_gamma, __set_gamma)
 
     def __str__(self):
-        return f"""The neuron network has {N} neurones with \
-               neurons' leakage coefficient of {gamma} and \
-               transmission lost coefficient of {beta}."""
+        return f"""The neuron network has {self.N} neurones with \
+               neurons' leakage coefficient of {self.gamma} and \
+               transmission lost coefficient of {self.beta}."""
 
     def __repr__(self):
         return """---In the first implemented model of a neural network,
@@ -100,12 +101,13 @@ class SimplifiedModel:
                 send nor receive signals in a step, its potential will not be totally 
                 preserved to the next step but be leaked. To simplify the decrease of potential,
                 we define a coefficient gamma as a decline linear factor to measure 
-                this leakage. In a step of a millisecond, a neuron i leaks 
+                this leakage. In a step of a millisecond, a neuron i preserve 
                 (gamma * V(i, t)) mV. Biologically, k is between the interval [0.9, 0.95].\n
                 In bref, the potential of neuron i at step t+1 is equal to the sum of its
                 "leftover" potential from the preceding step and the weighted sum of potential
                 of all neuron that can send signals to it.\n
-                V(i, t+1) = f(gamma * (1 - d(i, t)) * V(i,t) + beta * sum(d(k, t) * V(k, t)))
+                # V(i, t+1) = f(gamma * (1 - d(i, t)) * (V(i,t) + beta * sum(d(k, t) * V(k, t))))
+                V(i, t+1) = f((1 - d(i, t) * (gamma * V(i, t) + beta * sum(d(k, t) * V(k, t))))
                 where k is of all neurons that transmit to neuron i."""
 
     def init_system_links(self):
@@ -116,7 +118,8 @@ class SimplifiedModel:
         syst_links[i][j] = 0: j doesnt connect to i
         syst_links[i][i] = gamma/beta where gamma is the factor of leaking potential
         of the neuron i."""
-        return [[random.choice([0.0, 1.0]) if j != i else gamma / beta for j in range(N)] for i in range(N)]
+        return [[random.choice([0.0, 1.0]) if j != i else self.gamma / self.beta
+                 for j in range(self.N)] for i in range(self.N)]
 
     def init_syst_potential(self):
         """Create a matrix of 2D that represents the potential of each neuron at
@@ -136,26 +139,27 @@ class SimplifiedModel:
         After release all its potential, syst_state[i][0] = 0 at the next step"""
         return [0 for i in range(self.N)]
 
-    def matrix_Ni(self, i):
+    def matrix_Ni(self, i: int):
         """Create a matrix which helps keeping the potential of
         each neuron of the previous step to the next step.
         Size: (N,)
         Only the i of the neuron in current execution is set to 1.
         All the others elements are 0."""
-        return [1 if _i == i else 0 for _i in range(N)]
+        return [1 if _i == i else 0 for _i in range(self.N)]
 
-    def func_act(self, val_poten):
+    def func_act(self, val_poten: float):
         """float => float
         If the potential of a neuron is superior than the threshold, it's activated.
         When a neuron is activated, its potential increase immediately to Vmax"""
-        return val_poten if val_poten < threshold else Vmax
+        return val_poten if val_poten < SimplifiedModel.threshold else SimplifiedModel.Vmax
 
     def start_syst(self):
         """Send in the information in form electric ranged between 0 and Vmax (mV)
          to kick off the system."""
-        for i in range(N):
-            self.syst_potential[i][i] = self.func_act(self.syst_potential[i][i] + random.uniform(0.0, Vmax))
-            self.syst_state[i] = 1 if self.syst_potential[i][i] >= threshold else 0
+        for i in range(self.N):
+            self.syst_potential[i][i] = self.func_act(
+                self.syst_potential[i][i] + random.uniform(0.0, SimplifiedModel.Vmax))
+            self.syst_state[i] = 1 if self.syst_potential[i][i] >= SimplifiedModel.threshold else 0
 
     def update_system_one_step(self):
         """Calculate the potentials of all the neurons at the time t+1 and also update
@@ -164,21 +168,47 @@ class SimplifiedModel:
         Update the potentials and their states of the whole system in form matrix."""
         new_potential = deepcopy(self.syst_potential)
         new_state = deepcopy(self.syst_state)
-        for i in range(N):
+        for i in range(self.N):
+            # new_potential[i][i] = self.func_act(
+            #     self.beta * np.dot(
+            #         self.syst_links[i],
+            #         np.dot(
+            #             self.syst_potential,
+            #             np.add(
+            #                 self.syst_state,
+            #                 np.dot((-1) ** self.syst_state[i], self.matrix_Ni(i)))
+            #         )))
+            new_potential[i][i] = (1 - self.syst_state[i]) * self.func_act(
+                sum([self.beta * self.syst_links[i][j] * self.syst_potential[j][j] * self.syst_state[j]
+                     for j in range(N)]))
+            new_state[i] = 1 if new_potential[i][i] >= SimplifiedModel.threshold else 0
+        self.syst_state = new_state
+        self.syst_potential = new_potential
 
-            var = np.dot(syst_links[i], np.dot(syst_potentiel, syst_state + matrix_Ni(i, syst_state[i][0])))[0]
-            var = func_act(var)
-            if var == beta * Vmax:
-                new_syst_state[i][0] = 1
+    def non_transmittable(self):
+        """Verify if there is no neuron that can transmit signal to others
+        Return a bool"""
+        return all([x == 0 for x in self.syst_state])
+
+    def simulation(self, nb_steps: int):
+        """Return a list of all the matrixes, each matrix shows the potentials of the system
+        at moment t"""
+        for i in range(nb_steps):
+            if self.non_transmittable():
+                self.start_syst()
             else:
-                new_syst_state[i][0] = 0
-
-            new_syst_potentiel[i][i] = var
-
-        return (new_syst_potentiel, new_syst_state)
-
+                self.update_system_one_step()
+            yield (self.syst_state.copy(), self.syst_potential.copy())
 
 
 if __name__ == '__main__':
+    N = 5
+    gamma = 0.9
+    beta = 0.65
     model = SimplifiedModel(N, gamma, beta)
-    print(model.__repr__())
+    i = 0
+    print("link=\n", model.syst_links)
+    for p in model.simulation(10):
+        i+=1
+        state, potential = p
+        print(i, np.array(potential), "\n", state, "\n")
